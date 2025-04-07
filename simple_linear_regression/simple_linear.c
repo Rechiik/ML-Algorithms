64fe45c3dc9f114888278ed886fbb7d4017651d8
Rechiik <itz6dwaffle@gmail.com>
2025-04-07 18:08:02 +0000

    simple_linear_regression added

---
+++ simple_linear_regression/simple_linear.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct{
+	float m;
+	float c;
+} regressionEq;
+
+float hypoFunc(regressionEq eq, float x){
+	float c = eq.c;
+	float m = eq.m;
+	float y = m*x + c;
+	return y;
+}
+
+void gradientDescent(regressionEq *eq, float x[], float Y[], int m, float lr){
+	float dJ_dtheta0 = 0;
+	float dJ_dtheta1 = 0;
+	for (int i=0;i<m;i++){
+		dJ_dtheta0 += hypoFunc(*eq,x[i]) - Y[i];
+		dJ_dtheta1 += (hypoFunc(*eq,x[i]) - Y[i])*x[i];
+	}
+	eq->m = eq->m - lr*dJ_dtheta1;
+	eq->c = eq->c - lr*dJ_dtheta0;
+
+}
+
+float costFunction(regressionEq eq, float x[], float Y[], int m){
+	float cost = 0;
+	for (int i=0;i<m;i++){
+		float yPred = hypoFunc(eq,x[i]);
+		float yActual = Y[i];
+		cost += pow((yPred - yActual),2);	
+	}
+	cost = cost/ (2 * m);
+	return cost;
+}
+
+
+int main(){
+	regressionEq eq = {.c = 1.5,.m=1};
+	float x[] = {0,1,2,3,4,5};
+	float Y[] = {4,6,8,10,12,14};
+	int m = 6;
+	float cost = costFunction(eq, x, Y, m);
+	int epochs = 100;
+	int counter = 0;
+	printf("Cost at epoch %d: %f\n",counter,cost);
+	while (counter<epochs){
+		gradientDescent(&eq, x, Y, m, 0.01);
+		counter++;
+		cost = costFunction(eq, x, Y,m);
+		printf("Cost at epoch %d: %f\n",counter,cost);
+		printf("Value of 0_0: %f\nValue of 0_1: %f\n",eq.c,eq.m);
+	}
+	return 0;
+}
